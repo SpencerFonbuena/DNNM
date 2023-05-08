9dbfff0e322ba75db5c79191b4f6938e7be7ca20
SpencerFonbuena <108028784+SpencerFonbuena@users.noreply.github.com>
2023-05-08 10:56:15 -0600

    Update create_dataset.py
    checking the device

--- Gated_Transfomer_Network/data_process/create_dataset.py
+++ Gated_Transfomer_Network/data_process/create_dataset.py
@@ -29,11 +29,11 @@ class Create_Dataset(Dataset):
             window_set.append(np.expand_dims(example, 0))
         
         #Training Dataset  
-        self.dataset = torch.tensor(np.vstack(window_set)).transpose(-1,-2).to(DEVICE)
+        self.dataset = torch.tensor(np.vstack(window_set)).transpose(-1,-2).get_device()
         
         #Create the training labels. The reason it is starting from window size, is because there is technically labels for what happened after each timestep: however,
         #We created windows of data, so we want to know what is happening at the end of our window. If we started at the beginning, our labels would be off by the size of window_size
-        self.labels = torch.tensor(self.df['Labels'][window_size:].to_numpy()).to(DEVICE)
+        self.labels = torch.tensor(self.df['Labels'][window_size:].to_numpy()).get_device()
 
         #This is the stratified k fold index so that we can train and test on the whole set. The startified will also keep the label distribution constant in train and test
         self.sfk_indexset = StratifiedKFold(n_splits=c.k_fold, shuffle=True) \
