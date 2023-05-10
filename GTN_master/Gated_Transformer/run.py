aed62358a8e876248e45f9a1850378ea45df2cc2
SpencerFonbuena <schfonbuena@gmail.com>
2023-05-09 21:15:26 -0600

    try again

--- GTN_master/Gated_Transformer/run.py
+++ GTN_master/Gated_Transformer/run.py
@@ -45,7 +45,6 @@ BATCH_SIZE = 128
 LR = 1e-4
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")  # select device CPU or GPU
 print(f'use device: {DEVICE}')
-
 d_model = 512
 d_hidden = 1024
 q = 8
