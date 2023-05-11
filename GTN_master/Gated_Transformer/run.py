eec3a87ae73a3e2447ef7e311ec57b173fb45190
SpencerFonbuena <schfonbuena@gmail.com>
2023-05-10 19:19:06 -0600

    try again

--- GTN_master/Gated_Transformer/run.py
+++ GTN_master/Gated_Transformer/run.py
@@ -41,7 +41,7 @@ draw_key = 1  # Greater than or equal to draw_key will save the image
 file_name = path.split('\\')[-1][0:path.split('\\')[-1].index('.')]  # get file name
 
 # hyperparameter settings
-EPOCH = 40
+EPOCH = 100
 BATCH_SIZE = 128
 LR = 1e-6
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")  # select device CPU or GPU
