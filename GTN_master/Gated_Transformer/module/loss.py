dfd0ce5dd70a93fd68631883d8bee950ca53a195
SpencerFonbuena <schfonbuena@gmail.com>
2023-05-11 10:24:55 -0600

    on the same device

--- GTN_master/Gated_Transformer/module/loss.py
+++ GTN_master/Gated_Transformer/module/loss.py
@@ -2,13 +2,16 @@ from torch.nn import Module
 import torch
 from torch.nn import CrossEntropyLoss
 
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")  # select device CPU or GPU
+print(f'use device: {DEVICE}')
+
 class Myloss(Module):
     def __init__(self):
         super(Myloss, self).__init__()
         self.loss_function = CrossEntropyLoss()
 
     def forward(self, y_pre, y_true):
-        y_true = y_true.type(torch.FloatTensor)
+        y_true = y_true.type(torch.FloatTensor).to(DEVICE)
         loss = self.loss_function(y_pre, y_true)
 
         return loss
