5caee2036aeabf012daba74acaafff0f3a2990e8
SpencerFonbuena <schfonbuena@gmail.com>
2023-05-19 16:03:10 -0600

    adding headers

--- mach1/run.py
+++ mach1/run.py
@@ -29,7 +29,7 @@ DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")  # selec
 print(f'use device: {DEVICE}')
 
 wandb.init(
-    project='test full 1hour',
+    project='testing 1hour',
     name='Overfit1'
 )
 
