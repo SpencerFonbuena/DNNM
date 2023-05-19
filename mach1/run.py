0e0ae83ddcd22aec3048cc6537a27abe587a7619
SpencerFonbuena <schfonbuena@gmail.com>
2023-05-19 09:41:15 -0600

    making the model much smaller

--- mach1/run.py
+++ mach1/run.py
@@ -49,8 +49,8 @@ samplertrain = wrs(weights=train_dataset.trainsampleweights, num_samples=len(tra
 samplertest = wrs(weights=test_dataset.testsampleweights, num_samples=len(test_dataset), replacement=True)
 
 #Load the data
-train_dataloader = DataLoader(dataset=train_dataset, batch_size=hp.BATCH_SIZE, shuffle=False, num_workers=2)
-test_dataloader = DataLoader(dataset=test_dataset, batch_size=hp.BATCH_SIZE, shuffle=False, num_workers=2)
+train_dataloader = DataLoader(dataset=train_dataset, batch_size=hp.BATCH_SIZE, shuffle=False, num_workers=2, sampler=samplertrain)
+test_dataloader = DataLoader(dataset=test_dataset, batch_size=hp.BATCH_SIZE, shuffle=False, num_workers=2, sampler=samplertest)
 
 DATA_LEN = train_dataset.training_len # Number of samples in the training set
 d_input = train_dataset.input_len # number of time parts
