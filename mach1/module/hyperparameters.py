3652a41c68e077b7e29d4f5dfe06479870bc6a68
SpencerFonbuena <schfonbuena@gmail.com>
2023-05-20 12:03:45 -0600

    making a huge learning rate

--- mach1/module/hyperparameters.py
+++ mach1/module/hyperparameters.py
@@ -3,14 +3,14 @@ class HyperParameters:
     # hyperparameter settings
     EPOCH = 225
     BATCH_SIZE = 16
-    WINDOW_SIZE = 360
+    WINDOW_SIZE = 120
     LR = 5
     d_model = 512
     d_hidden = 1024
     q = 8
     v = 8
     h = 8
-    N = 32
+    N = 24
     dropout = 0.2
     split = .85
     pe = True # # The setting is in the twin towers score=pe score=channel has no pe by default
