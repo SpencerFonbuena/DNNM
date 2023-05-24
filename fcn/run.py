1ce447549859d88c9ee6702fe5982e52654a6b0e
SpencerFonbuena <schfonbuena@gmail.com>
2023-05-24 12:41:01 -0600

    new location

--- fcn/run.py
+++ fcn/run.py
@@ -24,7 +24,7 @@ DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")  # selec
 print(f'use device: {DEVICE}')
 
 wandb.init(
-    project='mach test',
+    project='mach fcn',
     name='test'
 )
 # [End Initialization]
