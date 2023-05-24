7444040f3c72aecba5c5d701f9a9f990d86e8796
SpencerFonbuena <schfonbuena@gmail.com>
2023-05-24 13:42:43 -0600

    try again

--- fcn/modules/fcn.py
+++ fcn/modules/fcn.py
@@ -4,16 +4,30 @@ import torch.nn.functional as F
 import numpy as np
 
 class FCN(nn.Module):
-    def __init__(self, data_in, data_out, layers=[128, 256, 128], kss=[8, 5, 3]):
+    def __init__(self, data_in, data_out, layers=[128, 256, 512], kss=[8, 5, 3]):
         super().__init__()
 
         self.conv1 = nn.Conv1d(data_in, layers[0], kss[0], 1, 3)
-        self.conv2 = nn.Conv1d(layers[0], layers[1], kss[1], 1, 2)
-        self.conv3 = nn.Conv1d(layers[1], layers[2], kss[2], 1, 1)
+        self.conv2 = nn.Conv1d(layers[0], layers[0], kss[0], 1, 2)
+        self.conv3 = nn.Conv1d(layers[0], layers[0], kss[0], 1, 2)
+        self.conv4 = nn.Conv1d(layers[0], layers[0], kss[0], 1, 2)
+        self.conv5 = nn.Conv1d(layers[0], layers[1], kss[1], 1, 2)
+        self.conv6 = nn.Conv1d(layers[1], layers[1], kss[1], 1, 2)
+        self.conv7 = nn.Conv1d(layers[1], layers[1], kss[1], 1, 2)
+        self.conv8 = nn.Conv1d(layers[1], layers[2], kss[2], 1, 2)
+        self.conv9 = nn.Conv1d(layers[2], layers[2], kss[2], 1, 2)
+        self.convout = nn.Conv1d(layers[2], layers[2], kss[2], 1, 1)
         
         self.bn1 = nn.BatchNorm1d(layers[0])
-        self.bn2= nn.BatchNorm1d(layers[1])
-        self.bn3 = nn.BatchNorm1d(layers[2])
+        self.bn2= nn.BatchNorm1d(layers[0])
+        self.bn3 = nn.BatchNorm1d(layers[0])
+        self.bn4 = nn.BatchNorm1d(layers[0])
+        self.bn5= nn.BatchNorm1d(layers[1])
+        self.bn6 = nn.BatchNorm1d(layers[1])
+        self.bn7 = nn.BatchNorm1d(layers[1])
+        self.bn8= nn.BatchNorm1d(layers[2])
+        self.bn9 = nn.BatchNorm1d(layers[2])
+        self.bnout = nn.BatchNorm1d(layers[2])
 
         self.gap = nn.AdaptiveAvgPool1d(1)
 
@@ -26,6 +40,13 @@ class FCN(nn.Module):
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = F.relu(self.bn5(self.conv5(x)))
+        x = F.relu(self.bn6(self.conv6(x)))
+        x = F.relu(self.bn7(self.conv7(x)))
+        x = F.relu(self.bn8(self.conv8(x)))
+        x = F.relu(self.bn9(self.conv9(x)))
+        x = F.relu(self.bnout(self.convout(x)))
         x = self.gap(x)
         x = x.reshape(x.shape[0], -1)
         x = self.fc(x)
