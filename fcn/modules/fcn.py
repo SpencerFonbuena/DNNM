a674b0abe2860ee7512fcaaf00f9c7bdcd40c857
SpencerFonbuena <schfonbuena@gmail.com>
2023-05-25 09:17:52 -0600

    more epochs, bigger filter

--- fcn/modules/fcn.py
+++ fcn/modules/fcn.py
@@ -17,6 +17,14 @@ class FCN(nn.Module):
         self.conv7 = nn.Conv1d(layers[1], layers[1], kss[0], 1, 3)
         self.conv8 = nn.Conv1d(layers[1], layers[1], kss[0], 1, 3)
         self.conv9 = nn.Conv1d(layers[1], layers[1], kss[0], 1, 3)
+        self.conv10 = nn.Conv1d(layers[1], layers[1], kss[0], 1, 3)
+        self.conv11 = nn.Conv1d(layers[1], layers[1], kss[0], 1, 3)
+        self.conv12 = nn.Conv1d(layers[1], layers[1], kss[0], 1, 3)
+        self.conv13 = nn.Conv1d(layers[1], layers[1], kss[0], 1, 3)
+        self.conv14 = nn.Conv1d(layers[1], layers[1], kss[0], 1, 3)
+        self.conv15 = nn.Conv1d(layers[1], layers[1], kss[0], 1, 3)
+        self.conv16 = nn.Conv1d(layers[1], layers[1], kss[0], 1, 3)
+        self.conv17 = nn.Conv1d(layers[1], layers[1], kss[0], 1, 3)
         self.convout = nn.Conv1d(layers[1], layers[1], kss[0], 1, 3)
         
         self.bn1 = nn.BatchNorm1d(layers[1])
@@ -28,42 +36,64 @@ class FCN(nn.Module):
         self.bn7 = nn.BatchNorm1d(layers[1])
         self.bn8= nn.BatchNorm1d(layers[1])
         self.bn9 = nn.BatchNorm1d(layers[1])
+        self.bn10 = nn.BatchNorm1d(layers[1])
+        self.bn11 = nn.BatchNorm1d(layers[1])
+        self.bn12= nn.BatchNorm1d(layers[1])
+        self.bn13 = nn.BatchNorm1d(layers[1])
+        self.bn14 = nn.BatchNorm1d(layers[1])
+        self.bn15= nn.BatchNorm1d(layers[1])
+        self.bn16 = nn.BatchNorm1d(layers[1])
+        self.bn17 = nn.BatchNorm1d(layers[1])
         self.bnout = nn.BatchNorm1d(layers[1])
-
         self.gap = nn.AdaptiveAvgPool1d(1)
 
-        self.fc = nn.Linear(layers[2], data_out)
+        self.fc = nn.Linear(layers[1], data_out)
 
 
         
     def forward(self, x):
         x = x.transpose(-1,-2)
         x = F.relu(self.bn1(self.conv1(x)))
-        print(x.shape)
         identity = x
-        
         x = F.relu(self.bn2(self.conv2(x)))
-        print(x.shape)
         x = F.relu(self.bn3(self.conv3(x)))
-        print(x.shape)
+        
         x = x + identity
         identity = x
         x = F.relu(self.bn4(self.conv4(x)))
-        print(x.shape)
         x = F.relu(self.bn5(self.conv5(x)))
-        print(x.shape)
+        
         x = x + identity
         identity = x
-        
         x = F.relu(self.bn6(self.conv6(x)))
-        print(x.shape)
         x = F.relu(self.bn7(self.conv7(x)))
-        print(x.shape)
+        
         x = x + identity
         identity = x
         x = F.relu(self.bn8(self.conv8(x)))
         x = F.relu(self.bn9(self.conv9(x)))
+        
         x = x + identity
+        identity = x
+        x = F.relu(self.bn10(self.conv10(x)))
+        x = F.relu(self.bn11(self.conv11(x)))
+        
+        x = x + identity
+        identity = x
+        x = F.relu(self.bn12(self.conv12(x)))
+        x = F.relu(self.bn13(self.conv13(x)))
+        x = x + identity
+        
+        identity = x
+        x = F.relu(self.bn14(self.conv14(x)))
+        x = F.relu(self.bn15(self.conv15(x)))
+        x = x + identity
+        
+        identity = x
+        x = F.relu(self.bn16(self.conv16(x)))
+        x = F.relu(self.bn17(self.conv17(x)))
+        x = x + identity
+        
         identity = x
         x = F.relu(self.bnout(self.convout(x)))
         x = self.gap(x)
