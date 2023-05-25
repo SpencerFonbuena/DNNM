f74e5bbf9c5f46bba40512b97d148f773e2413fb
SpencerFonbuena <schfonbuena@gmail.com>
2023-05-25 10:16:12 -0600

    bigger batch size and window

--- mach1/module/feedForward.py
+++ mach1/module/feedForward.py
@@ -10,17 +10,9 @@ random.seed(seed)
 torch.manual_seed(seed)
 
 class FeedForward(Module):
-    def __init__(self,
-                 d_model: int,
-                 d_hidden: int = 512):
+    def __init__():
         super(FeedForward, self).__init__()
-
-        self.linear_1 = torch.nn.Linear(d_model, d_hidden)
-        self.linear_2 = torch.nn.Linear(d_hidden, d_model)
 
     def forward(self, x):
 
-        x = self.linear_1(x)
-        x = F.relu(x)
-        x = self.linear_2(x)
-        return x
+        return 
--- mach1/module/multiHeadAttention.py
+++ mach1/module/multiHeadAttention.py
@@ -10,55 +10,8 @@ random.seed(seed)
 torch.manual_seed(seed)
 
 class MultiHeadAttention(Module):
-    def __init__(self,
-                 d_model: int,
-                 q: int,
-                 v: int,
-                 h: int,
-                 device: str,
-                 mask: bool=False,
-                 dropout: float = 0.0):
+    def __init__(self):
         super(MultiHeadAttention, self).__init__()
 
-        self.W_q = torch.nn.Linear(d_model, q * h)
-        self.W_k = torch.nn.Linear(d_model, q * h)
-        self.W_v = torch.nn.Linear(d_model, v * h)
-
-        self.W_o = torch.nn.Linear(v * h, d_model)
-
-        self.device = device
-        self._h = h
-        self._q = q
-
-        self.mask = mask
-        self.dropout = torch.nn.Dropout(p=dropout)
-        self.score = None
-
-    def forward(self, x, stage):
-            #print('input time step:', x.shape)
-        Q = torch.cat(self.W_q(x).chunk(self._h, dim=-1), dim=0)
-            #print('Q:', Q.shape)
-        K = torch.cat(self.W_k(x).chunk(self._h, dim=-1), dim=0)
-            #print('K:', K.shape)
-        V = torch.cat(self.W_v(x).chunk(self._h, dim=-1), dim=0)
-            #print('V:', V.shape)
-
-        #This creates the attention, which means that self.score is the un-softmaxed attnention weights
-        score = torch.matmul(Q, K.transpose(-1, -2)) / math.sqrt(self._q)
-            #print('Queries times Keys:', score.shape)
-        self.score = score
-
-        if self.mask and stage == 'train':
-            mask = torch.ones_like(score[0])
-            mask = torch.tril(mask, diagonal=0)
-            score = torch.where(mask > 0, score, torch.Tensor([-2**32+1]).expand_as(score[0]).to(self.device))
-
-        score = F.softmax(score, dim=-1)
-            #print('softmaxed values: ',score.shape)
-        attention = torch.matmul(score, V)
-            #print("Attention Values: ",attention.shape)
-        attention_heads = torch.cat(attention.chunk(self._h, dim=0), dim=-1)
-            #print('Attention Heads: ',attention_heads.shape)
-        self_attention = self.W_o(attention_heads)
-            #print('Output Shape:', self_attention.shape) # (16,120,512) | The output is the same as the input, it's just been encoded
-        return self_attention, self.score
+    def forward():
+        return 
