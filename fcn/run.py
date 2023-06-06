1a0dabd25a0e908c8cb38a6ea1d1b3506bf93551
SpencerFonbuena <schfonbuena@gmail.com>
2023-06-06 00:03:37 -0600

    modularizing

--- fcn/run.py
+++ fcn/run.py
@@ -59,7 +59,7 @@ test_dataloader = DataLoader(dataset=test_dataset, batch_size=hp.BATCH_SIZE, shu
 
 # [Initialize functions for training]
 loss_function = Myloss()
-model = FCN(data_in=9, data_out=4, layers=[128,256,512], kss=[7,5,3], stack=40,device=DEVICE,p=.5).to(DEVICE)
+model = FCN(data_in=9, data_out=4, layers=[128,256,512], kss=[7,5,3], stack=40,p=.5).to(DEVICE)
 
 #Choose Optimizer
 if hp.optimizer_name == 'Adam':
