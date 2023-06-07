2d6973ad217f1d4133209aa144c6306264f858ce
SpencerFonbuena <schfonbuena@gmail.com>
2023-06-06 20:39:10 -0600

    stochastic depth with gtn fcn

--- mach1/module/hyperparameters.py
+++ mach1/module/hyperparameters.py
@@ -4,13 +4,13 @@ class HyperParameters:
     EPOCH = 1500
     BATCH_SIZE = 32
     WINDOW_SIZE = 120
-    LR = .0003
+    LR = .03
     d_model = 512
     d_hidden = 2048
     queries = 8 # Queries
     values = 8 # Values
     heads = 8 # Heads
-    N = 1 # multi head attention layers
+    N = 8 # multi head attention layers
     dropout = 0.0
     split = .85
     optimizer_name = 'Adam'
