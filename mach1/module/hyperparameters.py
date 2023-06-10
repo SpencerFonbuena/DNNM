6a2eab56a70ef82c5af18f9d733dc58739ecee20
SpencerFonbuena <schfonbuena@gmail.com>
2023-06-10 12:27:49 -0600

    got rid of embedding tanh

--- mach1/module/hyperparameters.py
+++ mach1/module/hyperparameters.py
@@ -3,14 +3,14 @@ class HyperParameters:
     # hyperparameter settings
     EPOCH = 1500
     BATCH_SIZE = 36
-    WINDOW_SIZE = 240
+    WINDOW_SIZE = 120
     LR = .0003
     d_model = 512
     d_hidden = 2048
     queries = 8 # Queries
     values = 8 # Values
     heads = 8 # Heads
-    N = 24 # multi head attention layers
+    N = 16 # multi head attention layers
     dropout = 0.0
     split = .85
     optimizer_name = 'AdamW'
