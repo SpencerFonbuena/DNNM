c915c7508c57cdccb3bfe47d4e2eab613140744e
SpencerFonbuena <schfonbuena@gmail.com>
2023-06-12 20:31:35 -0600

    changing dataset to SPY and got rid of norm'

--- mach1/module/hyperparameters.py
+++ mach1/module/hyperparameters.py
@@ -10,11 +10,11 @@ class HyperParameters:
     queries = 8 # Queries
     values = 8 # Values
     heads = 16 # Heads
-    N = 16 # multi head attention layers
+    N = 24 # multi head attention layers
     dropout = 0.0
     split = .85
     optimizer_name = 'AdamW'
     clip = .9
-    p = .5
+    p = .01
     fcnstack = 2
     logs = 5
