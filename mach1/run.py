579c3d0738b45a1d79e0727d41847a5ef3953583
SpencerFonbuena <schfonbuena@gmail.com>
2023-06-23 09:03:19 -0600

    good og gating

--- mach1/run.py
+++ mach1/run.py
@@ -193,9 +193,6 @@ def train(config=None):
                 loss.backward()
                 torch.nn.utils.clip_grad_norm_(net.parameters(), .5)
                 optimizer.step()
-                
-                if 1 % 1000 == 0:
-                    print(net.channel_tower.linear2.weight.grad)
 
                 trainmetricaccuracy.update(y_pre, y)
                 specacc.update(y_pre.to(torch.int64), y.to(torch.int64))
