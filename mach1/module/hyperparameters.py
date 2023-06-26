7953bf8c57e606d4a5d703ce6e85949f288d4025
SpencerFonbuena <schfonbuena@gmail.com>
2023-06-26 09:27:49 -0600

    changed dataset and added stochastic depth

--- mach1/module/hyperparameters.py
+++ mach1/module/hyperparameters.py
@@ -8,9 +8,9 @@ class HyperParameters:
     d_hidden = [2048]
     queries = 8 # Queries
     values = 8 # Values
-    heads = [128] # Heads
+    heads = [64] # Heads
     N = [96] # multi head attention layers
-    dropout = [0.2]
+    dropout = [0.0]
     split = .85
     optimizer_name = 'AdamW'
     clip = .9
