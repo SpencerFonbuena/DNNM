2fb1f0b2393d3d0d166c1fa93ca000e5fdf3b5d0
SpencerFonbuena <schfonbuena@gmail.com>
2023-06-26 17:48:24 -0600

    smaller network

--- mach1/module/hyperparameters.py
+++ mach1/module/hyperparameters.py
@@ -9,7 +9,7 @@ class HyperParameters:
     queries = 8 # Queries
     values = 8 # Values
     heads = [64] # Heads
-    N = [32] # multi head attention layers
+    N = [16] # multi head attention layers
     dropout = [0.7]
     split = .85
     optimizer_name = 'AdamW'
