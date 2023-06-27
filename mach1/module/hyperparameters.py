49a82bfd3640a80dcb10db3707b813bae6927c33
SpencerFonbuena <schfonbuena@gmail.com>
2023-06-27 16:07:12 -0600

    8 test with stoch drop sweep

--- mach1/module/hyperparameters.py
+++ mach1/module/hyperparameters.py
@@ -10,7 +10,7 @@ class HyperParameters:
     values = 8 # Values
     heads = [64] # Heads
     N = [8] # multi head attention layers
-    dropout = [0.8]
+    dropout = [0.7]
     split = .85
     optimizer_name = 'AdamW'
     clip = .9
