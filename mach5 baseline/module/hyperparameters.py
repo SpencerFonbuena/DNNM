efec4e150604de0ca98d480b483ff9ad307cb84a
SpencerFonbuena <schfonbuena@gmail.com>
2023-07-04 02:02:11 -0600

    mach34 02 | changing back to normalized, with destationary factors

--- mach5 baseline/module/hyperparameters.py
+++ mach5 baseline/module/hyperparameters.py
@@ -9,7 +9,7 @@ class HyperParameters:
     queries = 8 # Queries
     values = 8 # Values
     heads = 64 # Heads
-    stack = 8 # multi head attention layers
+    stack = 32 # multi head attention layers
     dropout = 0.7
     split = .85
     optimizer_name = 'AdamW'
