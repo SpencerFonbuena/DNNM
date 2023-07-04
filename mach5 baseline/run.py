2d46704c417f2f7933b66f128b5fabecab6c5fb1
SpencerFonbuena <schfonbuena@gmail.com>
2023-07-04 00:38:12 -0600

    mach33 03 | Changed the dataset_process file and changed back to gaus

--- mach5 baseline/run.py
+++ mach5 baseline/run.py
@@ -83,9 +83,9 @@ wandb.init(project='mach33', name='01')
 
 #switch datasets depending on local or virtual run
 if torch.cuda.is_available():
-    path = '/root/DNNM/mach1/datasets/SPY_30mins_gaus.txt'
+    path = '/root/DNNM/mach1/datasets/SPY_30mins_returns.txt'
 else:
-    path = 'DNNM/mach1/datasets/SPY_30mins_gaus.txt'
+    path = 'DNNM/mach1/datasets/SPY_30mins_returns.txt'
 
 # [End General Init]
 
