2ac432cd59992e0e2cbf81b20c9ab35a6064f445
SpencerFonbuena <schfonbuena@gmail.com>
2023-07-06 14:40:52 -0600

    mach42 01| testing the one ahead forecast model

--- mach4/module/hyperparameters.py
+++ mach4/module/hyperparameters.py
@@ -2,16 +2,16 @@ from sklearn.preprocessing import StandardScaler
 
 class HyperParameters:
     # hyperparameter settings
-    EPOCH = 2
-    batch_size = 128
-    window_size = 240
+    EPOCH = 10
+    batch_size = 150
+    window_size = 120
     LR = .00001 #.00001,.00003,.00005,.00007,.00009
     d_model = 512
     d_hidden = 2048
     queries = 8 # Queries
     values = 8 # Values
     heads = 64 # Heads
-    stack = 8 # multi head attention layers
+    stack = 16 # multi head attention layers
     dropout = 0.7
     split = .85
     optimizer_name = 'AdamW'
