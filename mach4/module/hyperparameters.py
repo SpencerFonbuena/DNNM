283b61a0cd12ff2ad14c3674a4402f4b6bd34643
SpencerFonbuena <schfonbuena@gmail.com>
2023-07-06 17:44:44 -0600

    mach43 11 | 512 batch| .00001 LR| 128 window| 8 stack

--- mach4/module/hyperparameters.py
+++ mach4/module/hyperparameters.py
@@ -3,13 +3,13 @@ from sklearn.preprocessing import StandardScaler
 class HyperParameters:
     # hyperparameter settings
     EPOCH = 10
-    batch_size = 64
-    window_size = 60
+    batch_size = 256
+    window_size = 120
     LR = .00001
     d_model = 512
     d_hidden = 2048
     heads = 64 
-    stack = 128 
+    stack = 8
     dropout = 0.7
     split = .85
     clip = .9
