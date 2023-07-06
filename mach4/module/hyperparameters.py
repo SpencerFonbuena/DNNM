d861a28a4276ded27eebdc81f9dcb0d4b540af2f
SpencerFonbuena <schfonbuena@gmail.com>
2023-07-06 16:39:13 -0600

    mach43 04 | batch 256 too big, changed to 128 with .00001 learning rate and 256 window

--- mach4/module/hyperparameters.py
+++ mach4/module/hyperparameters.py
@@ -3,7 +3,7 @@ from sklearn.preprocessing import StandardScaler
 class HyperParameters:
     # hyperparameter settings
     EPOCH = 10
-    batch_size = 256
+    batch_size = 128
     window_size = 256
     LR = .00001
     d_model = 512
