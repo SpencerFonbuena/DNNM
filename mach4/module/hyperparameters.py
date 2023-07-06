2a458dfbfd07ca8a709810609dc7c0f480029719
SpencerFonbuena <schfonbuena@gmail.com>
2023-07-06 17:08:49 -0600

    mach43 08 | batch 64 with .00001 learning rate and 60 window 16 stack

--- mach4/module/hyperparameters.py
+++ mach4/module/hyperparameters.py
@@ -4,12 +4,12 @@ class HyperParameters:
     # hyperparameter settings
     EPOCH = 10
     batch_size = 64
-    window_size = 30
+    window_size = 60
     LR = .00001
     d_model = 512
     d_hidden = 2048
     heads = 64 
-    stack = 8 
+    stack = 16 
     dropout = 0.7
     split = .85
     clip = .9
