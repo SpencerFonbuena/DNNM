1ef8c1c7c635f1515edf4cfabb631ebeb17bc443
SpencerFonbuena <schfonbuena@gmail.com>
2023-07-09 16:03:31 -0600

    creating new architecture

--- mach5 baseline/run.py
+++ mach5 baseline/run.py
@@ -78,7 +78,7 @@ print(f'use device: {DEVICE}')
 # [End Sweeps]
 
 # Log on Weights and Biases
-wandb.init(project='mach46', name='01')
+wandb.init(project='mach47', name='01')
 
 #switch datasets depending on local or virtual run
 if torch.cuda.is_available():
