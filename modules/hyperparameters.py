4b1dc176944cb80cb596434a4e6f88e32c017c8f
SpencerFonbuena <schfonbuena@gmail.com>
2023-07-12 11:48:48 -0600

    big push

--- modules/hyperparameters.py
+++ modules/hyperparameters.py
@@ -3,14 +3,14 @@ class Hyperparameters:
     pred_size = 24
     lookback = 168
     seg_len = 6
-    batch_size = 32
+    batch_size = 48
     split = .7
     if torch.cuda.is_available():
         num_workers = 2
     else:
         num_workers = 1
     data_dim = 8
-    learning_rate = .0003
+    learning_rate = .00005
     win_size = 2
     factor = 10
     d_model = 512
